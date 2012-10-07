dedc68b3fc02c7273bf54e993d69d5f4c368e439
Simon Turner <turners8n@hotmail.com>
2012-10-07 13:04:36 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HopeTest::Application.config.secret_token = '23f396af539a1a03b64a8f344d2238e865f0bfa7a249d14efdb306194477584528fdaacb6d7265377c9318e8e134f8f7942be76e3e7b95a38f4a0fde70769974'
