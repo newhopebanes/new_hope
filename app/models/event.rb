1f6aa1e13f5c887c738f6173e968e42ced1a4682
Simon Turner <turners8n@hotmail.com>
2012-10-08 06:54:55 +0100

    Update

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,7 @@
 class Event < ActiveRecord::Base
-  attr_accessible :name, :organisation, :description, :contact_person, :contact_role, :phone, :email, :address, :contact_id, :date, :time, :ongoing, :frequency_id, :day_id, :cost, :access, :access_details, :referral_id, :joining_process, :target_ids, :tag_ids, :directions_walking, :directions_bus, :directions_train, :directions_car, :other
-  has_many :tag
-  has_many :target
+  attr_accessible :name, :organisation, :description, :contact_person, :contact_role, :phone, :email, :address, :contact_id, :date, :time, :ongoing, :frequency_id, :day_id, :cost, :access, :access_details, :referral_id, :joining_process, :target_id, :tag_id, :directions_walking, :directions_bus, :directions_train, :directions_car, :other
+  belongs_to :tag
+  belongs_to :target
   belongs_to :day
   belongs_to :frequency
   belongs_to :referral
@@ -10,9 +10,8 @@ class Event < ActiveRecord::Base
   validates :name, :presence => true
   validates :organisation,  :presence => true
   validates :cost, :presence => true
-  #validates :access,  :presence => true
+  validates :access,  :presence => true
   validates :referral_id, :presence => true
   validates :target_id,  :presence => true
-  
 
 end
