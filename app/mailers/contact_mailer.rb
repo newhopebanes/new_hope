27b48c09278c14c7a35ebdefcbea4ebcdf46366e
simon turner <turners8n@hotmail.com>
2013-01-13 11:38:52 +0000

    Deleted files related to old email implimentation

--- app/mailers/contact_mailer.rb
+++
@@ -1,8 +0,0 @@
-class ContactMailer < ActionMailer::Base
-  default from: "newhope.webapp@gmail.com"
-  
-  def query_mail(message)
-    @message = message
-    mail(:to => "turners8n@hotmail.com", :subject => @message.subject)
-  end
-end
