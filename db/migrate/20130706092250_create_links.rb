bbed8c02b88e2a3e9e6278e3d1d6ade44252d631
simon turner <turners8n@hotmail.com>
2013-07-07 18:06:08 +0100

    add nil to text size limit (for postgres)

--- db/migrate/20130706092250_create_links.rb
+++ db/migrate/20130706092250_create_links.rb
@@ -6,7 +6,7 @@ class CreateLinks < ActiveRecord::Migration
       t.string :phone
       t.string :location
       t.string :opening
-      t.text :text
+      t.text :text, :limit => nil
       t.integer :link_type_id
 
       t.timestamps
